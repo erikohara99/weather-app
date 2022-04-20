0a421c56218d09958390ae0f189a7bd1e29de866
Erik <slicedblue@gmail.com>
2022-04-19 20:34:14 -0400

    added layout for weather component per city

--- src/components/WeatherSlab.jsx
+++ src/components/WeatherSlab.jsx
@@ -1,6 +1,18 @@
 function WeatherSlab(props) {
+
     return ( 
-        <h1>Test</h1>
+        <div className="weather-container">
+            <h3>San Francisco, CA</h3>
+            <h3>Sunny</h3>
+            <h3>Date</h3>
+            <div className="weather-temps">
+                <h1>21°</h1>
+                <div>
+                    <h2>25°</h2>
+                    <h2>18°</h2>
+                </div>
+            </div>
+        </div>
      );
 }
 
