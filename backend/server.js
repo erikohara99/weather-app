8ddee7ab9c52d352c852d62ab0f6f091c4aef599
Erik <slicedblue@gmail.com>
2022-04-21 22:09:03 -0400

    added searchbar and fixed api searching, //todo: render results

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const app = require("express")();
 const axios = require("axios");
+const cors = require("cors");
 
-app.get("/api/weather", (req, res) => {
-    const {zipcode} = req.body;
-    let query = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},840&appid=${process.env.openweathermap_ApiKey}`;
-    query = axios.get(query);
-    res.send(query);
+app.use(cors());
+
+app.get("/api/weather", async (req, res) => {
+
+    // Get coords of zip code provided (US only, can change by making the 'US' query a variable)
+    const {zipcode} = req.query;
+    let query = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},US&appid=${process.env.openweathermap_ApiKey}`;
+    let coords = await axios.get(query);
+
+    // Get weather of town from coordinates
+    query = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.data.lat}&lon=${coords.data.lon}&units=imperial&appid=${process.env.openweathermap_ApiKey}`;
+    let weather = await axios.get(query);
+    res.send(weather.data);
 });
 
 app.listen(3000, () => {console.log("Server is listening on port 3000...")});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import SearchBar from './components/SearchBar';
 import WeatherSlab from "./components/WeatherSlab";
+const axios = require("axios");
 
 function App() {
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const zipcode = document.getElementById("zipsearch").value;
+    const result = await axios.get(`http://localhost:3000/api/weather?zipcode=${zipcode}`);
+    console.log(result);
+  }
 
   const weather = {
     temp: 21,
@@ -11,6 +20,7 @@ function App() {
 
   return (
     <div className="App">
+      <SearchBar onSubmit={handleSubmit}/>
       <WeatherSlab 
         location="San Francisco, CA" 
         weather={weather}
